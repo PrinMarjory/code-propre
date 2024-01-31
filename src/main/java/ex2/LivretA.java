931873cd377378078cc39dc914696ac7bec38db3
Marjory PRIN <marjory.prin@gmail.com>
2024-01-31 10:46:56 +0100

    Exo 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,49 @@
 package ex2;
 
+/** Représente un compte bancaire de type livret A 
+ * @author Marjory PRIN
+ */
 public class LivretA extends CompteBancaire {
+	
+	/** tauxRemuneration : taux de rémunération */
+	private double tauxRemuneration;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/** Constructeur
+	 * @param type
+	 * @param solde
+	 * @param tauxRemuneration
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, 0);
+		this.tauxRemuneration = tauxRemuneration;
 	}
-
+	
+	public void appliquerRemuAnnuelle(){
+		double solde = this.getSolde();
+		this.setSolde(solde + solde*tauxRemuneration/100);
+	}
+	
+	/** Ajoute un montant au solde
+	 * @param montant
+	 */
+	@Override
+	public void debiterMontant(double montant){
+		if (this.getSolde() - montant > 0){
+			this.setSolde(this.getSolde()- montant); 
+		}	
+	}
+	
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
 }
